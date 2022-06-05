9239657fac072c28ceb0d1fa7e3a31e48f8899a7
cow1234 <79946779+cow1234@users.noreply.github.com>
2022-06-04 20:56:57 -0500

    Add files via upload

---
+++ t.js
@@ -0,0 +1,20 @@
+function something(){
+    document.getElementById("for").style.background="red";
+    document.getElementById("back").style.background="yellow";
+    document.getElementById("back").style.textAlign="center";   
+    document.getElementById("two").style.width="400px";    
+    document.getElementById("two").style.height="400px";   
+}
+function something2() {
+    document.getElementById("for").style.background="blue";
+    document.getElementById("back").style.background="purple";
+    document.getElementById("back").style.textAlign="left";     
+    document.getElementById("for").style.width="400px";    
+    document.getElementById("for").style.height="400px";   
+}
+function something3(){
+    document.getElementById("for").style.width="40px";    
+    document.getElementById("for").style.height="40px";
+    document.getElementById("two").style.width="1000px";    
+    document.getElementById("two").style.height="720px";   
+}
